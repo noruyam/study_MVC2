69ae6693d022c620d7955aba8bc3f2aa945febba
KCC <qkr2633911@naver.com>
2022-11-18 11:31:18 +0900

    최초

---
+++ webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.newlecture.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		 
+//		ModelAndView mv = new ModelAndView("/WEB-INF/view/index.jsp");// mv.setViewName("/WEB-INF/view/index.jsp");
+		ModelAndView mv = new ModelAndView("index"); //dispatcher-servlet.xml에 InternalResourceViewResolver 설정하면 됨
+		mv.addObject("data","Hello world@@@");
+		return mv;
+	}
+
+}
