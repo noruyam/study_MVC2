8023b4a0946947ec47a757f00483a25529e59c41
KCC <qkr2633911@naver.com>
2022-11-24 11:43:15 +0900

    Annotation으로 URL 매핑하기

--- webprj/src/main/java/com/newlecture/web/controller/IndexController.java
+++ webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -1,21 +1,25 @@
 package com.newlecture.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+@Controller
+public class IndexController { //implements Controller{
 
-public class IndexController implements Controller{
+//	@Override
+//	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		// TODO Auto-generated method stub
+//		 
+////		ModelAndView mv = new ModelAndView("/WEB-INF/view/index.jsp");// mv.setViewName("/WEB-INF/view/index.jsp");
+//		ModelAndView mv = new ModelAndView("root.index"); //dispatcher-servlet.xml에 InternalResourceViewResolver 설정하면 됨
+//		mv.addObject("data","Hello world@@@");
+//		return mv;
+//	}
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		 
-//		ModelAndView mv = new ModelAndView("/WEB-INF/view/index.jsp");// mv.setViewName("/WEB-INF/view/index.jsp");
-		ModelAndView mv = new ModelAndView("root.index"); //dispatcher-servlet.xml에 InternalResourceViewResolver 설정하면 됨
-		mv.addObject("data","Hello world@@@");
-		return mv;
+	@RequestMapping("/index")
+	public void aaaa() {
+		System.out.println("Test");
+		
 	}
-
+	
 }
