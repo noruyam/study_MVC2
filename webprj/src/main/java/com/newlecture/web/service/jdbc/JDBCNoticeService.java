3beb203e6981ded92936b0d16211f2a41272ab2e
KCC <qkr2633911@naver.com>
2022-11-24 11:31:07 +0900

    Annotation으로 서비스 객체 생성하기

--- webprj/src/main/java/com/newlecture/web/service/jdbc/JDBCNoticeService.java
+++ webprj/src/main/java/com/newlecture/web/service/jdbc/JDBCNoticeService.java
@@ -13,11 +13,15 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.newlecture.web.entity.Help;
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
 
+
+@Service //@Component
 public class JDBCNoticeService implements NoticeService {
 
 //	private String url = "jdbc:oracle:thin:@localhost:1521/orcl";
