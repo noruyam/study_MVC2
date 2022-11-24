32e9afe8ac6fb54acf25a6534d61971a24d08349
KCC <qkr2633911@naver.com>
2022-11-24 10:32:53 +0900

    연결정보 분리하기

--- webprj/src/main/java/com/newlecture/web/service/h2/H2NoticeService.java
+++ webprj/src/main/java/com/newlecture/web/service/h2/H2NoticeService.java
@@ -10,129 +10,121 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import com.newlecture.web.entity.Help;
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
 
 public class H2NoticeService implements NoticeService {
 
-		
-	 private String url = "jdbc:h2:tcp://localhost/~/test"; 
-	 private String uid = "sa"; 
-	 private String pwd = ""; 
-	 private String driver = "org.h2.Driver";
-		 
+//	private String url = "jdbc:h2:tcp://localhost/~/test";
+//	private String uid = "sa";
+//	private String pwd = "";
+//	private String driver = "org.h2.Driver";
 
-	  public List<Help> getHelpList(int page, String field, String query) throws ClassNotFoundException, SQLException {
-		  int start = 1 + (page-1)*10;     // 1, 11, 21, 31, ..
-			int end = 10*page; // 10, 20, 30, 40...
-			
-			String sql = "SELECT * FROM HELP WHERE "+field+" LIKE ? AND SEQ BETWEEN ? AND ?";	
-			
-			Class.forName(driver);
-			Connection con = DriverManager.getConnection(url,uid, pwd);
-			PreparedStatement st = con.prepareStatement(sql);
-			st.setString(1, "%"+query+"%");
-			st.setInt(2, start);
-			st.setInt(3, end);
-			ResultSet rs = st.executeQuery();
-			
-			List<Help> list = new ArrayList<Help>();
-			
-			while(rs.next()){
-			    String title = rs.getString("TOPIC");
-			    int id = rs.getInt("SEQ");
-			    String writerId = rs.getString("INFO");
-			    
-			    
-			    Help help = new Help(
-			    					title,
-			    					id,
-			    					writerId
-			    				);
+	private DataSource dataSource;
+	
 
-			    list.add(help);
-			    
-			}
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
-			
-			rs.close();
-			st.close();
-			con.close();
-			
-			return list;
-		}
-		  
-	  
-	  
-	  
-	public List<Notice> getList(int page, String field, String query) throws ClassNotFoundException, SQLException{
-		
-		int start = 1 + (page-1)*10;     // 1, 11, 21, 31, ..
-		int end = 10*page; // 10, 20, 30, 40...
-		
-		String sql = "SELECT * FROM NOTICE WHERE "+field+" LIKE ? AND id BETWEEN ? AND ?";	
-		
-		Class.forName(driver);
-		Connection con = DriverManager.getConnection(url,uid, pwd);
+	public List<Help> getHelpList(int page, String field, String query) throws ClassNotFoundException, SQLException {
+		int start = 1 + (page - 1) * 10; // 1, 11, 21, 31, ..
+		int end = 10 * page; // 10, 20, 30, 40...
+
+		String sql = "SELECT * FROM HELP WHERE " + field + " LIKE ? AND SEQ BETWEEN ? AND ?";
+
+//		Class.forName(driver);
+//		Connection con = DriverManager.getConnection(url, uid, pwd);
+		Connection con = dataSource.getConnection();
 		PreparedStatement st = con.prepareStatement(sql);
-		st.setString(1, "%"+query+"%");
+		st.setString(1, "%" + query + "%");
 		st.setInt(2, start);
 		st.setInt(3, end);
 		ResultSet rs = st.executeQuery();
-		
-		List<Notice> list = new ArrayList<Notice>();
-		
-		while(rs.next()){
-		    int id = rs.getInt("ID");
-		    String title = rs.getString("TITLE");
-		    String writerId = rs.getString("WRITER_ID");
-		    Date regDate = rs.getDate("REGDATE");
-		    String content = rs.getString("CONTENT");
-		    int hit = rs.getInt("hit");
-		    String files = rs.getString("FILES");
-		    
-		    Notice notice = new Notice(
-		    					id,
-		    					title,
-		    					writerId,
-		    					regDate,
-		    					content,
-		    					hit,
-		    					files
-		    				);
 
-		    list.add(notice);
-		    
+		List<Help> list = new ArrayList<Help>();
+
+		while (rs.next()) {
+			String title = rs.getString("TOPIC");
+			int id = rs.getInt("SEQ");
+			String writerId = rs.getString("INFO");
+
+			Help help = new Help(title, id, writerId);
+
+			list.add(help);
+
 		}
 
-		
 		rs.close();
 		st.close();
 		con.close();
-		
+
 		return list;
 	}
-	
-	// Scalar 
-	public int getCount() throws ClassNotFoundException, SQLException {
-		int count = 0;
-		
-		String sql = "SELECT COUNT(ID) COUNT FROM NOTICE";	
-		
-		Class.forName(driver);
-		Connection con = DriverManager.getConnection(url,uid, pwd);
-		Statement st = con.createStatement();
-		
-		ResultSet rs = st.executeQuery(sql);
-		
-		if(rs.next())
-			count = rs.getInt("COUNT");		
-		
+
+	public List<Notice> getList(int page, String field, String query) throws ClassNotFoundException, SQLException {
+
+		int start = 1 + (page - 1) * 10; // 1, 11, 21, 31, ..
+		int end = 10 * page; // 10, 20, 30, 40...
+
+		String sql = "SELECT * FROM NOTICE WHERE " + field + " LIKE ? AND id BETWEEN ? AND ?";
+
+//		Class.forName(driver);
+//		Connection con = DriverManager.getConnection(url, uid, pwd);
+		Connection con = dataSource.getConnection();
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, "%" + query + "%");
+		st.setInt(2, start);
+		st.setInt(3, end);
+		ResultSet rs = st.executeQuery();
+
+		List<Notice> list = new ArrayList<Notice>();
+
+		while (rs.next()) {
+			int id = rs.getInt("ID");
+			String title = rs.getString("TITLE");
+			String writerId = rs.getString("WRITER_ID");
+			Date regDate = rs.getDate("REGDATE");
+			String content = rs.getString("CONTENT");
+			int hit = rs.getInt("hit");
+			String files = rs.getString("FILES");
+
+			Notice notice = new Notice(id, title, writerId, regDate, content, hit, files);
+
+			list.add(notice);
+
+		}
+
 		rs.close();
 		st.close();
 		con.close();
-		
+
+		return list;
+	}
+
+	// Scalar
+	public int getCount() throws ClassNotFoundException, SQLException {
+		int count = 0;
+
+		String sql = "SELECT COUNT(ID) COUNT FROM NOTICE";
+
+//		Class.forName(driver);
+//		Connection con = DriverManager.getConnection(url, uid, pwd);
+		Connection con = dataSource.getConnection();
+		Statement st = con.createStatement();
+
+		ResultSet rs = st.executeQuery(sql);
+
+		if (rs.next())
+			count = rs.getInt("COUNT");
+
+		rs.close();
+		st.close();
+		con.close();
+
 		return count;
 	}
 
@@ -141,85 +133,77 @@ public class H2NoticeService implements NoticeService {
 		String writerId = notice.getWriterId();
 		String content = notice.getContent();
 		String files = notice.getFiles();
-		
+
 //		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
-		String sql = "INSERT INTO notice (    " + 
-				"    title," + 
-				"    writer_id," + 
-				"    content," + 
-				"    files" + 
-				") VALUES (?,?,?,?)";	
-		
-		Class.forName(driver);
-		Connection con = DriverManager.getConnection(url,uid, pwd);                   
-		//Statement st = con.createStatement();
-		//st.ex....(sql)
+		String sql = "INSERT INTO notice (    " + "    title," + "    writer_id," + "    content," + "    files"
+				+ ") VALUES (?,?,?,?)";
+
+//		Class.forName(driver);
+//		Connection con = DriverManager.getConnection(url, uid, pwd);
+		Connection con = dataSource.getConnection();
+		// Statement st = con.createStatement();
+		// st.ex....(sql)
 		PreparedStatement st = con.prepareStatement(sql);
 		st.setString(1, title);
 		st.setString(2, writerId);
 		st.setString(3, content);
 		st.setString(4, files);
-		
+
 		int result = st.executeUpdate();
-		
-		
+
 		st.close();
 		con.close();
-		
+
 		return result;
 	}
-	
+
 	public int update(Notice notice) throws SQLException, ClassNotFoundException {
 		String title = notice.getTitle();
 		String content = notice.getContent();
 		String files = notice.getFiles();
 		int id = notice.getId();
-		
+
 //		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
-		String sql = "UPDATE NOTICE " + 
-				"SET" + 
-				"    TITLE=?," + 
-				"    CONTENT=?," + 
-				"    FILES=?" + 
-				"WHERE ID=?";
-		
-		Class.forName(driver);
-		Connection con = DriverManager.getConnection(url,uid, pwd);                   
-		//Statement st = con.createStatement();
-		//st.ex....(sql)
+		String sql = "UPDATE NOTICE " + "SET" + "    TITLE=?," + "    CONTENT=?," + "    FILES=?" + "WHERE ID=?";
+
+//		Class.forName(driver);
+//		Connection con = DriverManager.getConnection(url, uid, pwd);
+		Connection con = dataSource.getConnection();
+		// Statement st = con.createStatement();
+		// st.ex....(sql)
 		PreparedStatement st = con.prepareStatement(sql);
 		st.setString(1, title);
 		st.setString(2, content);
 		st.setString(3, files);
 		st.setInt(4, id);
-		
+
 		int result = st.executeUpdate();
-				
+
 		st.close();
 		con.close();
-		
-		return result;
-	}
-	
-	public int delete(int id) throws ClassNotFoundException, SQLException {
-	
-//		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
-		String sql = "DELETE NOTICE WHERE ID=?";
-		
-		Class.forName(driver);
-		Connection con = DriverManager.getConnection(url,uid, pwd);                  
-		//Statement st = con.createStatement();
-		//st.ex....(sql)
-		PreparedStatement st = con.prepareStatement(sql);		
-		st.setInt(1, id);
-		
-		int result = st.executeUpdate();
-				
-		st.close();
-		con.close();
-		
+
 		return result;
 	}
 
-	
+	public int delete(int id) throws ClassNotFoundException, SQLException {
+
+//		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		String sql = "DELETE NOTICE WHERE ID=?";
+
+//		Class.forName(driver);
+//		Connection con = DriverManager.getConnection(url, uid, pwd);
+		Connection con = dataSource.getConnection();
+		// Statement st = con.createStatement();
+		// st.ex....(sql)
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, id);
+
+		int result = st.executeUpdate();
+
+		st.close();
+		con.close();
+
+		return result;
+	}
+
 }
